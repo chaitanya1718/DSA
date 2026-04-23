c8998aab72ab38226ce0dabdc818b1a6ca8e042c
Chaitanya <chaitanyag1718@gmail.com>
2026-04-23 12:22:00 +0530

    L2615

---
+++ SumOfDistancesL2615.java
@@ -0,0 +1,30 @@
+class SumOfDistancesL2615{
+
+    // O(n^2) solution
+    static long[] brute_distance(int[] nums) {
+
+        int n=nums.length;
+        long res[]=new long[n];
+
+        for(int i=0;i<n;i++){
+            long sum=0;
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                if(nums[i]==nums[j]){
+                    sum+=Math.abs(i-j);
+                }
+            }
+            res[i]=sum;
+        }
+
+        return res;
+        
+    }
+
+    // optimal
+
+
+    public static void main(String[] args) {
+        
+    }
+}
